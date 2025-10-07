0b8e06be4c4ef261e67790f92b8db4a53df4caf0
dcarjim0101 <dcarjim0101@g.educaand.es>
2025-10-07 10:29:48 +0200

    pedir un numero y contar hasta el, con el bucle for

--- contarhasta.py
+++ contarhasta.py
@@ -1,6 +1,11 @@
 n = int(input("introduce un numero: "))
 
+i = 0
 
 for i in range(n):
-    print(i + 1)
+    if i < n:
+            print(i, end=",")
+    else:
+        break
     i += 1
+print(i)
