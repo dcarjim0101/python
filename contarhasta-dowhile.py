405d21c355504f1c18abaf49437f17a109128eb1
dcarjim0101 <dcarjim0101@g.educaand.es>
2025-10-07 09:36:28 +0200

    pedir un numero y contar hasta el, usando el bucle do while

--- contarhasta-dowhile.py
+++ contarhasta-dowhile.py
@@ -3,8 +3,9 @@ n = int(input("introduce un numero: "))
 i = 1
 
 while True:
-    if i <= n:
-        print(i)
+    if i < n:
+        print(i, end=",")
     else:
         break
     i += 1
+print(i)
