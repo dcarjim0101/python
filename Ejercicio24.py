a2d1816ff381406bddfd61af854ab3fe591d768b
dcarjim0101 <dcarjim0101@g.educaand.es>
2025-10-08 11:04:29 +0200

    sumar numeros hasta pulsar 0 y terminar el programa

---
+++ Ejercicio24.py
@@ -0,0 +1,8 @@
+suma = 0
+
+n = int(input("introduce un numero: "))
+
+while n != 0:
+    n = int(input("introduce otro numero o pulsa 0 para terminar: "))
+    suma += n
+print("la suma total  es: ", suma)
